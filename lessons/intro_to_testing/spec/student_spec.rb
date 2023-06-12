fb2d406154aeab71975047e1782085f25d1c185a
Charles Ren <charlesren99@gmail.com>
2023-06-12 11:07:08 -0700

    Finished Intro_to_testing. Cannot figure out how to run successfully. File path error?

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,33 @@
 # Add the 2 requires you will need here
+require 'rspec'
+require 'lib/student.rb'
 
-
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
       # write the code to initialize a new student object
+      student = Student.new('Penelope')
+      expect(student).to be_a Student
     end
+
     # test it has a name
+    it 'has a name' do
+      student = Student.new('Penelope')
+      expect(student.name).to eq 'Penelope'
+    end
+
     # test it has cookies
+    it 'has cookies' do
+      student = Student.new('Penelope')
+      expect(student.cookies).to be_an(Array)
+      expect(student.cookies).to be_empty
+    end
+
     # test it can add cookies
+    it 'can add cookies' do
+      student = Student.new('Penelope')
+      student.add_cookies('Chocolate Chip')
+      expect(student.cookies).to contain_exactly('Chocolate Chip')
+    end
   end
 end
