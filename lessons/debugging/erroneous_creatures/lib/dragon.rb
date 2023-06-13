74c1adc55cdcdffe0d1a047165f36321b53fad3b
Charles Ren <charlesren99@gmail.com>
2023-06-13 15:26:20 -0700

    Completed Erroneous_Creatures

--- lessons/debugging/erroneous_creatures/lib/dragon.rb
+++ lessons/debugging/erroneous_creatures/lib/dragon.rb
@@ -3,7 +3,7 @@ class Dragon
   attr_reader :name, :rider, :color
   attr_accessor :hungryness, :food_consumed, :fire
 
-  def initalize(name, color, rider)
+  def initialize(name, color, rider)
     @name = name
     @color = color
     @rider = rider
@@ -17,7 +17,7 @@ class Dragon
 
   def eat
    @food_consumed += 1
-   if food_consumed < 3
+   if @food_consumed < 3
      @hungryness = true
    else
      @hungryness = false
--- lessons/debugging/erroneous_creatures/lib/unicorn.rb
+++ lessons/debugging/erroneous_creatures/lib/unicorn.rb
@@ -3,18 +3,19 @@ class Unicorn
   attr_reader :name
   attr_accessor :color, :eating
 
-  def initialze(name, color="white")
+  def initialize(name, color="white")
     @name = name
     @color = color
     @eating = false
   end
 
   def white?
-    @colors == "white"
+    @color == "white"
   end
 
   def say(x)
     "**;* #{x} **;*"
+  end
 
   def fly
     if @eating
