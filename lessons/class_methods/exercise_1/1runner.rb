58286b76ba622fc862c3ed08f1bed762d6e36e5e
Charles Ren <charlesren99@gmail.com>
2023-06-24 21:23:21 -0700

    Complete 1runner.rb

--- lessons/class_methods/exercise_1/1runner.rb
+++ lessons/class_methods/exercise_1/1runner.rb
@@ -4,14 +4,17 @@ max = Dog.new("Max")
 puts max.woof
 
 ## Describe what each part of the code above did -
-# `max` =>
-# `Dog` =>
-# `.woof` =>
+# `max` => created a new instance of the 'Dog' class named max while passing in
+# the string "Max"
+# `Dog` => Dog refers to the class object
+# `.woof` => Method inside of Dog class called on the max object
 
 # puts Dog.woof
-
+# 'Woof!'
 ## Is there any difference between the two methods above? (max.woof and Dog.woof)?
 
 ## Un-comment the line of code below, does it work? Why or why not?
-# Dog.bark
+#Dog.bark
+# No as we did not specify the object we're calling it on.
+# It should be max.bark
 
