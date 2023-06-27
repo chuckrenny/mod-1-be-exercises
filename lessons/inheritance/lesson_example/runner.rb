474d18f747afabd71c3ef5c7e70f0093e9765736
Charles Ren <charlesren99@gmail.com>
2023-06-26 23:31:28 -0700

    Finish lessons/inheritance/lesson_example

--- lessons/inheritance/lesson_example/runner.rb
+++ lessons/inheritance/lesson_example/runner.rb
@@ -1,11 +1,18 @@
 require './lib/ceo'
 require './lib/manager'
 require './lib/intern'
+require './lib/manager'
+require './lib/ceo'
 
-ali = Ceo.new(15, 20)
-ben = Manager.new(10)
-chris = Intern.new(5)
+ali = Ceo.new(15, 20, "Daniel", 3349)
+ben = Manager.new(10, "Ben", 2009)
+chris = Intern.new(5, "Intern", 9999)
 
-require "pry"; binding.pry
+# require "pry"; binding.pry
 
-# Ali, Ben, and Chris all need names and IDs in the system. Where can they get those?
+# Ali, Ben, and Chris all need names and IDs in the system. 
+# Where can they get those?
+
+puts ali.bonus
+puts ben.name
+puts chris.id
--- lessons/inheritance/lesson_example/spec/ceo_spec.rb
+++ lessons/inheritance/lesson_example/spec/ceo_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Ceo do
 
   describe "setup" do
     it "exists" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new(15,20, "Daniel", 3349)
     end
     it "has base_salary and bonus attributes" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new(15,20, "Daniel", 3349)
       expect(ceo.base_salary).to eq(15)
       expect(ceo.bonus).to eq(20)
     end
@@ -15,7 +15,22 @@ RSpec.describe Ceo do
 
   describe "Has a name, ID, and total_compensation" do
     # Write tests to prove that a Ceo instance can have a Name and ID number, like any other Employee.
+    it 'has a name and id' do
+      ceo = Ceo.new(15, 20, "Daniel", 3349)
+      expect(ceo.name).to eq("Daniel")
+      expect(ceo.id).to eq(3349)
+    end
+
     # Also, the CEO should be able to receive a total compensation. Write at least one test that proves they can access this method.
+    it 'can access total compensation in employee parent class' do
+      ceo = Ceo.new(15, 20, "Daniel", 3349)
+      expect(ceo.total_compensation).to eq(35)
+    end
+
+    it 'can access benefits in employee parent class' do
+      ceo = Ceo.new(15, 20, "Daniel", 3349)
+      expect(ceo.benefits).to eq([:sick_leave])
+    end
   end
 
 end
