0e080c7d6bfc9f779a54dea0172348ef5f18a246
Charles Ren <cren04@syr.edu>
2023-07-13 02:04:09 -0700

    Finished potluck and tests

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -11,4 +11,36 @@ class Potluck
     def add_dish(item)
         @dishes << item
     end
+
+    def get_all_from_category(type)
+        @dishes.select do |dish|
+            dish.category == type
+        end
+    end
+
+    def menu
+        appetizers = get_all_from_category(:appetizer).map do |dish|
+            dish.name
+        end.sort
+        
+     
+
+        entres = get_all_from_category(:entre).map do |dish|
+            dish.name
+        end.sort
+
+        desserts = get_all_from_category(:dessert).map do |dish|
+            dish.name
+        end.sort
+
+        {
+            appetizers: appetizers,
+            entres: entres,
+            desserts: desserts
+        }
+    end
+
+    def ratio(type)
+        (get_all_from_category(type).length / dishes.length.to_f)*100
+    end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe Potluck do
 
   let(:couscous_salad)  {Dish.new("Couscous Salad", :appetizer)}
   let(:cocktail_meatballs)  {Dish.new("Cocktail Meatballs", :entre)}
-  
+  let(:summer_pizza) {Dish.new("Summer Pizza", :appetizer)}
+  let(:roast_pork) {Dish.new("Roast Pork", :entre)}
+  let(:cocktail_meatballs) {Dish.new("Cocktail Meatballs", :entre)}
+  let(:candy_salad) {Dish.new("Candy Salad", :dessert)}
+  let(:bean_dip) {Dish.new("Bean Dip", :appetizer)}
+
   describe '#initialize' do
     it 'is a potluck' do
         expect(potluck).to be_a(Potluck)
@@ -27,6 +32,55 @@ RSpec.describe Potluck do
 
       expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
       expect(potluck.dishes.length).to eq(2)
+    end
+  end
+
+  describe '#get_all_from_category' do
+    it 'can add dish to dishes' do
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(candy_salad)
+
+      expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs, summer_pizza, roast_pork, candy_salad])
+
+      expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
+
+      expect(potluck.get_all_from_category(:appetizer).first).to eq(couscous_salad)
+      expect(potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
+    end
+  end
+
+  describe '#menu' do
+    it 'can get the menu with categories sorted as keys and items as values' do
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(candy_salad)
+      potluck.add_dish(bean_dip)
+
+      menu_items = {
+        appetizers: ["Bean Dip", "Couscous Salad", "Summer Pizza"],
+        entres: ["Cocktail Meatballs", "Roast Pork"],
+        desserts: ["Candy Salad"]
+      }
+
+      expect(potluck.menu).to eq(menu_items)
+    end
+  end
+
+  describe '#ratio' do
+    it 'can find the ratio of appetizers to the course' do
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(candy_salad)
+      potluck.add_dish(bean_dip)
+
+      expect(potluck.ratio(:appetizer)).to eq(50.0)
     end
   end
 end
