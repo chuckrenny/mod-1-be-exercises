d420e1a5fbad2282d807f78617b82ac41acbdb42
Charles Ren <cren04@syr.edu>
2023-07-31 00:12:35 -0700

    feat: Iteration 4

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -13,4 +13,33 @@ class Dock
   def rent(boat, renter) 
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    dock_charge = {}
+
+    person = rental_log[boat]
+    hours = [boat.hours_rented, @max_rental_time].min
+
+    dock_charge[:card_number] = person.credit_card_number
+    dock_charge[:amount] = boat.price_per_hour * hours
+
+    dock_charge
+  end
+
+  def return(boat)
+    rental_log.delete(boat)
+  end
+
+  def log_hour
+    @rental_log.map do |boat, person|
+      boat.add_hour
+    end
+  end
+
+  def revenue
+    @rental_log.sum do |boat, person| 
+      charge(boat)[:amount] 
+    end
+  end
+  
 end
--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -41,4 +41,88 @@ RSpec.describe Dock do
     end
   end
 
+  describe "#charge" do
+    it 'can charge' do
+      @dock.rent(@kayak_1, @patrick)    
+      @dock.rent(@kayak_2, @patrick)    
+      @dock.rent(@sup_1, @eugene)  
+
+      @kayak_1.add_hour
+      @kayak_1.add_hour
+
+      dock_charge = {
+        :card_number => "4242424242424242",
+        :amount => 40
+      }
+
+      expect(@dock.charge(@kayak_1)).to eq(dock_charge)
+    end
+
+    it 'can limit the max hours' do
+      @dock.rent(@kayak_1, @patrick)    
+      @dock.rent(@kayak_2, @patrick)    
+      @dock.rent(@sup_1, @eugene)  
+
+      @kayak_1.add_hour
+      @kayak_1.add_hour
+      @kayak_1.add_hour
+      @kayak_1.add_hour
+
+      dock_charge = {
+        :card_number => "4242424242424242",
+        :amount => 60
+      }
+
+      expect(@dock.charge(@kayak_1)).to eq(dock_charge)
+    end
+  end
+
+  describe "#return" do
+    it 'can return the boat' do
+      @dock.rent(@sup_1, @eugene)  
+
+      @sup_1.add_hour
+      @sup_1.add_hour
+      @sup_1.add_hour
+
+      @dock.charge(@sup_1)
+
+      @dock.return(@sup_1)
+      expect(@dock.rental_log).to eq({})
+    end
+  end
+
+  describe "#log_hour" do
+    it 'can increment all rented boats by an additional hour' do
+      @dock.rent(@kayak_1, @patrick)    
+      @dock.rent(@kayak_2, @patrick)    
+      @dock.rent(@sup_1, @eugene)  
+
+      @kayak_1.add_hour
+      @kayak_2.add_hour
+      @sup_1.add_hour
+
+      @dock.log_hour
+
+      expect(@kayak_1.hours_rented).to eq(2)
+      expect(@kayak_2.hours_rented).to eq(2)
+      expect(@sup_1.hours_rented).to eq(2)
+    end
+  end
+
+  describe "#revenue" do
+    it 'returns the total revenue of all the boats' do
+      @dock.rent(@kayak_1, @patrick)    
+      @dock.rent(@kayak_2, @patrick)    
+      @dock.rent(@sup_1, @eugene)  
+
+      @kayak_1.add_hour
+      @kayak_2.add_hour
+      @sup_1.add_hour
+
+      @dock.log_hour
+      @dock.log_hour
+      expect(@dock.revenue).to eq(165)
+    end
+  end
 end
